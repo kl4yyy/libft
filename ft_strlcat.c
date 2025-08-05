9e257b2bf8c846b467ff63a618c4ed4d50e14086
Mohammad Najem <mnajem@c2r3s5.42amman.com>
2025-08-05 18:28:43 +0300

    h

---
+++ ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnajem <mnajem@amman.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 17:50:47 by mnajem            #+#    #+#             */
+/*   Updated: 2025/08/05 18:27:02 by mnajem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t strlcpy(char *dst, const char *src, size_t size){
+    int i;
+	int	y;
+	int	destlng;
+
+	i = 0;
+	destlng = 0;
+	while (dst[destlng] != '\0')
+	{
+		destlng++;
+	}
+	y = (size - destlng - 1);
+	while ((src[i] != '\0' && y))
+	{
+		dst[destlng] = src[y];
+		destlng++;
+		y--;
+		i++;
+	}
+	dst[destlng] = '\0';
+	return (destlng);
+}
