19161d8a8adc2a2d7a7069198aa9d3ff9928ef97
kl4y <103466413+kl4yyy@users.noreply.github.com>
2025-08-09 20:54:34 +0000

    Create ft_substr.c
    h

---
+++ ft_substr.c
@@ -0,0 +1,31 @@
+#include "libft.h"
+char *ft_substr(char const *s, unsigned int start,size_t len)
+{
+    size_t sln;
+    char *sub;
+    if(!s) return NULL;
+    sln = ft_strlen(s);
+    if(start>=sln) return ft_strdup("");
+
+    if (len > sln - start) len = sln - start;
+
+    sub = malloc(len+1);
+    if (!sub)return NULL;
+    sln = 0;
+    while(sln < len){
+        sub[sln] = s[start+sln];
+        sln++;
+    }
+    sub[sln] = '\0';
+    return (sub);
+
+}
+// int main(void)
+// {
+//     size_t len = 10;
+//     char *s = "hello";
+//     int start = 2;
+//     char *fun = ft_substr(s,start,len);
+//     printf("%s\n",fun);
+//     free(fun);
+// }
