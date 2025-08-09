12ce80252c79b2ea28ccbd4f17e1a33eeb16adaf
kl4y <103466413+kl4yyy@users.noreply.github.com>
2025-08-09 22:26:55 +0300

    ft_strdup.c
    h

---
+++ ft_strdup.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+char *strdup(const char *s)
+{
+    size_t i = 0;
+    char *dub;
+    while(s[i] != '\0') i++;
+
+    dub = malloc (i+1);
+    if(!dub)return NULL;
+    i = 0;
+    while(s[i] != '\0'){
+        dub[i] = s[i];
+        i++;
+    }
+    dub[i]='\0';
+    return (dub);
+}
+
+int main(void){
+    char *s = "hello";
+    char *fun = strdup(s);
+    printf("%s\n",fun);
+    free(fun);
+}
