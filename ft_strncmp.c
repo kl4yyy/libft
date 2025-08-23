0da61c52e77f549d9bd1c022d96ebc5a693290b9
kl4y <103466413+kl4yyy@users.noreply.github.com>
2025-08-23 09:10:04 +0300

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// check the codition
 #include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
