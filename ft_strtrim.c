5d28cc12f94952086a17d1ed413a54fdc87d1b08
kl4y <103466413+kl4yyy@users.noreply.github.com>
2025-08-23 09:10:31 +0300

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// check the logic
 #include "libft.h"
 
 char	*ft_strtrim(const char *s1, const char *set)
